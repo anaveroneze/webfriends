beb6b0f1c6c6e5aafca7b0b3b4d7c03b325142de
anaveroneze <ana_luisavs@hotmail.com>
2017-10-14 13:25:14 -0300

    registration view

--- webfriends/webfriends/regbackend.py
+++ webfriends/webfriends/regbackend.py
@@ -2,7 +2,7 @@
 from registration.backends.default.views import RegistrationView
 from experiment.forms import UsuarioFriendsForm
 from experiment.models import UsuarioFriends
-from registration.forms import RegistrationForm
+from django.http import HttpResponseRedirect
 
 class MyRegistrationView(RegistrationView):
 
