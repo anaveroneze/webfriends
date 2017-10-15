517b9eb867bc257610a7edaf1e92d85303c89d8f
anaveroneze <ana_luisavs@hotmail.com>
2017-10-14 22:27:58 -0300

    redirect to login interface after register

--- webfriends/webfriends/regbackend.py
+++ webfriends/webfriends/regbackend.py
@@ -2,7 +2,6 @@
 from registration.backends.default.views import RegistrationView
 from experiment.forms import UsuarioFriendsForm
 from experiment.models import UsuarioFriends
-from django.contrib import messages
 from django.http import HttpResponse
 from django.core.urlresolvers import reverse
 
@@ -10,12 +9,11 @@ class MyRegistrationView(RegistrationView):
 
 	form_class = UsuarioFriendsForm
 
-	def register(self,request, form_class):
-		new_user = super(MyRegistrationView,self).register(request,form_class)
+	def register(self, form_class):
+		new_user = super(MyRegistrationView,self).register(form_class)
 		user_profile = UsuarioFriends()
 		user_profile.usuario = new_user
 		user_profile.nickname = form_class.cleaned_data['nickname']
 		user_profile.company = form_class.cleaned_data['company']
 		user_profile.save()
-		messages.add_message(request, messages.INFO, 'Yeehaw!')
 		return HttpResponseRedirect(reverse('login'))
