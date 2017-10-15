f9e1f6d78279180e9ab186f44d30805898a9374b
anaveroneze <ana_luisavs@hotmail.com>
2017-10-15 01:12:27 -0200

    redirect to login interface after register

--- webfriends/webfriends/regbackend.py
+++ webfriends/webfriends/regbackend.py
@@ -19,6 +19,6 @@ class MyRegistrationView(RegistrationView):
 		user_profile.save()
 		return user_profile
 
-	def my_view(request):
-		messages.success(request, 'O email de confirmação foi enviado.')
-		return '/login/'
+	def get_success_url(self, user):
+		return render_to_response('login.html', message='Save complete')
+		#return '/login/'
