ea1ad443f4cbbd7781b785307c7f7de4e7ee6578
anaveroneze <ana_luisavs@hotmail.com>
2017-10-14 22:18:11 -0300

    putting message alert and login html

--- webfriends/webfriends/regbackend.py
+++ webfriends/webfriends/regbackend.py
@@ -10,12 +10,12 @@ class MyRegistrationView(RegistrationView):
 
 	form_class = UsuarioFriendsForm
 
-	def register(self, form_class):
-		new_user = super(MyRegistrationView,self).register(form_class)
+	def register(self,request, form_class):
+		new_user = super(MyRegistrationView,self).register(request,form_class)
 		user_profile = UsuarioFriends()
 		user_profile.usuario = new_user
 		user_profile.nickname = form_class.cleaned_data['nickname']
 		user_profile.company = form_class.cleaned_data['company']
 		user_profile.save()
-		messages.add_message(messages.INFO, 'Yeehaw!')
+		messages.add_message(request, messages.INFO, 'Yeehaw!')
 		return HttpResponseRedirect(reverse('login'))
