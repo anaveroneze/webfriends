ea77a7eb72325c60a5088864614b933c1ff957be
anaveroneze <ana_luisavs@hotmail.com>
2017-10-24 10:09:44 -0200

    Adding max file message

--- webfriends/experiment/forms.py
+++ webfriends/experiment/forms.py
@@ -4,15 +4,15 @@ from registration.forms import RegistrationFormUniqueEmail
 from .models import Algorithms
 from django.core.exceptions import ValidationError
 
-from django.shortcuts import render
-from django.template import RequestContext
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 #maximum size 25MB input file
 def file_size(value):
     limit = 26214400
     if value.size > limit:
         raise ValidationError('Arquivo muito grande. Tamanho máximo deve ser de 20MB.')
-        return render(request, "experiments.html", context)
+        return HttpResponseRedirect(reverse('experiments'))
 
 class UsuarioFriendsForm(RegistrationFormUniqueEmail):
     nickname = forms.CharField(required=False)
